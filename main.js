e7e2a92e07f2aee0f7cc5b521f36f8b8aaca4f18
Nilo <ndroock1@gmail.com>
2020-01-21 22:32:32 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("demo").innerHTML = "Hello World!";
+
+document.getElementById("para").innerHTML = "Dit is een zin.";
+
+document.write("Puinismen! by Japie FIK en Wim (from the RS tapes).")
